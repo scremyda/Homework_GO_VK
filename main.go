aba0cdf9fc8bd235224d1fbeab5d85f2c9c2a99c
Belozerov <belozerov.msk>
2023-09-19 21:28:12 +0300

    Code refactored and some logic from main moved

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"uniq/utilities"
 )
 
@@ -17,41 +16,27 @@ func main() {
 
 	argsWithoutFlags := flag.Args()
 
-	var inputFile *os.File
-	var outputFile *os.File
-	if len(argsWithoutFlags) > 0 && len(argsWithoutFlags) < 3 {
-		inputFile, err = os.Open(argsWithoutFlags[0])
-		if err != nil {
-			err = fmt.Errorf("can't open input file: %s", flag.Args()[0])
-			fmt.Println("Error happened: ", err)
+	inputFile, outputFile := utilities.OpenFiles(argsWithoutFlags)
+
+	defer func() {
+		if err = inputFile.Close(); err != nil {
+			err = fmt.Errorf("error closing file: %s", argsWithoutFlags[0])
+			fmt.Println("Error happened while closing file: ", err)
 			return
 		}
-		defer func() {
-			if err = inputFile.Close(); err != nil {
-				err = fmt.Errorf("error closing file: %s", flag.Args()[0])
-				fmt.Println("Error happened: ", err)
-				return
-			}
-		}()
-		if len(argsWithoutFlags) == 2 {
-			outputFile, err = os.Create(argsWithoutFlags[1])
-			if err != nil {
-				err = fmt.Errorf("can't create output file: %s", flag.Args()[1])
-				fmt.Println("Error happened: ", err)
-				return
-			}
-			defer func() {
-				if err = outputFile.Close(); err != nil {
-					err = fmt.Errorf("error closing file: %s", flag.Args()[1])
-					fmt.Println("Error happened: ", err)
-					return
-				}
-			}()
+	}()
+
+	defer func() {
+		if err = outputFile.Close(); err != nil {
+			err = fmt.Errorf("error closing file: %s", argsWithoutFlags[1])
+			fmt.Println("Error happened while closing file: ", err)
+			return
 		}
-	}
+	}()
+
 	inputData, err := utilities.GetData(inputFile)
 	if err != nil {
-		fmt.Println("Error happened: ", err)
+		fmt.Println("Error happened while reading: ", err)
 		return
 	}
 
@@ -59,7 +44,7 @@ func main() {
 
 	err = utilities.WriteData(outputFile, processedLines)
 	if err != nil {
-		fmt.Println("Error happened: ", err)
+		fmt.Println("Error happened while writing: ", err)
 		return
 	}
 }
